6b5961930ed6f8f998a4fae6d89f0c772a800523
donank <11donank22@gmail.com>
2021-01-27 18:31:44 +0530

    Added sign in and up buttons

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, Image, TextInput, Text, Button, StyleSheet, View, Dimensions } from "react-native";
-import { SignInButton } from "../components/signInButton";
-import { SignUpButton } from "../components/singUpButton";
+import { SafeAreaView, Image, TextInput, StyleSheet, View, Dimensions } from "react-native";
+import { SignInButton } from "../components/SignInButton";
+import { SignUpButton } from "../components/SignUpButton";
 
 const Login = () => {
   return (
